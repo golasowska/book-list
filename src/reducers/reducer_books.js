8cbc93f652cf7d4bf7f4acd948461bdf98870756
Anna Golasowska <annagolasowska@gmail.com>
2018-03-28 21:35:30 +0200

    added reducer and detail view

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
-    return [
-        { title: 'Book 1'},
-        { title: 'Book 2'},
-        { title: 'Book 3'},
-        { title: 'Book 4'}
-    ];
+  return [
+    { title: 'Book 1', pages: 23 },
+    { title: 'Book 2', pages: 11223 },
+    { title: 'Book 3', pages: 765 },
+    { title: 'Book 4', pages: 67 }
+  ];
 }
